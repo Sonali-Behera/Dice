e550a623ffe7dd8afbbda4876720e3b48eac89a6
Sonali Behera <68459369+Sonali-Behera@users.noreply.github.com>
2020-09-05 12:48:50 +0530

    Add files via upload
    Sixth project using HTML , CSS and JavaScript

---
+++ index.js
@@ -0,0 +1,17 @@
+var randomNumber1 = (Math.floor(Math.random()*6))+1;
+var randomNumber2 = (Math.floor(Math.random()*6))+1;
+var randomDiceName  = "images/dice"+randomNumber1+".png";
+document.querySelectorAll("img")[0].setAttribute("src",randomDiceName);
+var randomDiceName2  = "images/dice"+randomNumber2+".png";
+document.querySelectorAll("img")[1].setAttribute("src",randomDiceName2);
+if(randomDiceName===randomDiceName2)
+{
+  document.querySelector("h1").innerHTML="Match draw!";
+}
+else if (randomDiceName>randomDiceName2)
+{
+  document.querySelector("h1").innerHTML="🚩Play 1 wins!!!";
+}
+else {
+  document.querySelector("h1").innerHTML="Play 2 wins🚩!!!";
+}
