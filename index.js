ca5b1666f2f0df258bf2a5d54261d88df299cf1c
Sonali Behera <68459369+Sonali-Behera@users.noreply.github.com>
2020-12-21 17:16:54 +0530

    update user name adding feature

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let player1 = alert("Enter 1st player name:");
-let player2 = alert("Enter 2nd player name:");
+let player1 = prompt("Enter 1st player name:");
+let player2 = prompt("Enter 2nd player name:");
 
 var randomNumber1 = (Math.floor(Math.random()*6))+1;
 var randomNumber2 = (Math.floor(Math.random()*6))+1;
